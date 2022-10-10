965658e48a89ff7952ce1646dd621dac01d96dd0
gustavo-gomess <gusgegomes@hotmail.com>
2022-10-10 16:00:57 -0300

    fetat error

--- back/src/middlewares/notFound.ts
+++ back/src/middlewares/notFound.ts
@@ -3,6 +3,7 @@ import { CustomError } from "../models/customError";
 
 function notFound(req: Request, res: Response, next: NextFunction) {
   res.status(404);
+
   throw new CustomError("A URL informada é inválida", 404, "");
 }
 
